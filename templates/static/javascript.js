41ca718454b544e9a2020b27f5e3502a345343a3
Mustkass <mustkass@noreply.01.kood.tech>
2023-06-14 23:36:11 +0000

    JavaScript

--- templates/static/javascript.js
+++ templates/static/javascript.js
@@ -1,30 +1,42 @@
-var slider = document.getElementById("myRange");
-var output = document.getElementById("CreationYear");
-output.innerHTML = slider.value; // Display the default slider value
+const minRange = document.getElementById('minRange');
+const maxRange = document.getElementById('maxRange');
 
-// Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
-  output.innerHTML = this.value;
+minRange.addEventListener('input', updateSlider);
+maxRange.addEventListener('input', updateSlider);
+
+function updateSlider() {
+  const min = parseInt(minRange.value);
+  const max = parseInt(maxRange.value);
+
+  if (min > max) {
+    minRange.value = max;
+  } else if (max < min) {
+    maxRange.value = min;
+  }
 }
 
-var slider1 = document.getElementById("myRange2");
-var output1 = document.getElementById("CreationYear22");
+var slider1 = document.getElementById("minRange");
+var slider2 = document.getElementById("maxRange");
+var output1 = document.getElementById("CreationYearFrom");
+var output2 = document.getElementById("CreationYearTo");
 output1.innerHTML = slider1.value; // Display the default slider value
-
 // Update the current slider value (each time you drag the slider handle)
 slider1.oninput = function() {
   output1.innerHTML = this.value;
 }
-
+slider2.oninput = function() {
+  output2.innerHTML = this.value;
+}
 //-----------------
 function filter() {
-  if (document.getElementById("filter").style.display == "none") {
-      document.getElementById("filter").style.display = "block";
+  if (document.getElementById("filtr").style.display == "none") {
+      window.scrollTo(0, 0);
+      document.getElementById("filtr").style.display = "block";
   } else {
-      document.getElementById("filter").style.display = "none";
+      window.scrollTo(0, 0);
+      document.getElementById("filtr").style.display = "none";
   }
 }
-
 function rangeValue() {
   const value = document.querySelectorAll(".outYear")
   const input = document.querySelectorAll(".inYear")
@@ -34,4 +46,18 @@ function rangeValue() {
           value[index].textContent = event.target.value
       })
   }
+}
+
+var slider3 = document.getElementById("minnYear");
+var output3 = document.getElementById("fryear");
+var slider4 = document.getElementById("maxxYear");
+var output4 = document.getElementById("toyear");
+//output3.innerHTML = slider3.value; // Display the default slider value
+//output4.innerHTML = slider4.value;
+// Update the current slider value (each time you drag the slider handle)
+slider3.oninput = function() {
+  output3.innerHTML = this.value;
+}
+slider4.oninput = function() {
+  output4.innerHTML = this.value;
 }
